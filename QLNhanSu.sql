36dff10f03a531a37ff49522963dea726110f2ff
Ngocanh411997 <ngocanh411997@github.com>
2018-03-05 00:10:52 +0700

    Dữ liệu Nhân Viên

--- QLNhanSu.sql
+++ QLNhanSu.sql
@@ -1,4 +1,4 @@
-create database QLNhanSu
+﻿create database QLNhanSu
 go
 use QLNhanSu
 go
@@ -40,7 +40,18 @@ MaNV char(10) references NhanVien(MaNV),
 MaCV char(10) references ChucVu(MaChucVu),
 NgayNhanChuc date,
 primary key (MaNV,MaCV))
-
+--------- 04/03/2018-------
+go
+alter table NhanVien
+alter column GioiTinh varchar(3)
+go
+insert into NhanVien(MaNV,HoTen,DanToc,GioiTinh,SDT,QueQuan,NgaySinh)
+values ('NV01','Hoàng Thị Minh','kinh','Nu','0976986543','Hà Nội','09-08-1990'),
+('NV02','Nguyễn Quang Huy','kinh','Nam','0973686583','Vĩnh Phúc','10-19-1990'),
+('NV03','Ngô Hữu Huy','kinh','Nam','0976639201','Hà Nam','03-20-1993'),
+('NV04','Bùi Trung Kiên','kinh','Nam','0976863496','Hà Nội','12-08-1992'),
+('NV05','Nguyễn Thị Ngọc','kinh','Nu','01647386289','Phú Thọ','02-08-1991'),
+('NV0','Lê Bá Lộc','kinh','Nam','0976963984','Ha Noi','01-08-1995')
 
 
 
