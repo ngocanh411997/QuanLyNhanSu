e464ffb5120853534365170e59f60afe7511d053
ManhCuong9617 <36960151+ManhCuong9617@users.noreply.github.com>
2018-03-22 21:04:52 +0700

    model trinhdohocvan và chucvu

---
+++ QuanLyNhanSu/QuanLyNhanSu/Models/ChucVu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyNhanSu.Models
+{
+    class ChucVu
+    {
+        public string MaChucVu { get; set; }
+
+        public string TenChucVu { get; set; }
+
+        public ChucVu()
+        {
+            MaChucVu = "";
+            TenChucVu = "";
+        }
+        public ChucVu(string _MaChucVu, string _TenChucVu)
+        {
+            MaChucVu = _MaChucVu;
+            TenChucVu = _TenChucVu;
+        }
+    }
+}
